247aa52dc4d8e1d0d374e018efa62d6cd40ef475
Trey Wenger <tvwenger@gmail.com>
2021-01-20 10:41:42 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='WISP',
+    version='2.1',
+    description='Wenger Interferometry Software Package',
+    author='Trey V. Wenger',
+    author_email='tvwenger@gmail.com',
+    packages=['wisp'],
+    install_requires=['astropy'],
+)
